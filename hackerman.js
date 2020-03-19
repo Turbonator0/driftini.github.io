e0218954280b72c6575944fa6e74425cd9d8343c
VictiniTheDriftini <34460237+Driftini@users.noreply.github.com>
2020-03-19 01:54:34 +0100

    Create hackerman.js

---
+++ hackerman.js
@@ -0,0 +1,10 @@
+let topbarhtml = `<header class="topbar">
+            <div class="toptab">
+                <img class="icon" src="https://raw.githubusercontent.com/Driftini/tfs/master/favicon.ico">
+                <h1>THE FROGGY SHRINE <i class="smol">(aka Driftini's website)</i></h1>
+            </div>
+            <a class="toptext toptab" href="index.html"><span>Home</span></a>
+            <a class="toptext toptab" href="4m.html"><span>Project 4Modes</span></a>
+            <a class="toptext toptab" href="w103.html"><span>Win103</span></a>
+        </header>`;
+document.getElementById("barhere").innerHTML = topbarhtml;
