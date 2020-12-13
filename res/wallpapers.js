86435e7d90daffa635bafa7187d917057839fde0
Driftini <yurisilveos741@gmail.com>
2020-12-13 11:48:12 +0100

    Fixes and new wallpapers

--- res/wallpapers.js
+++ res/wallpapers.js
@@ -1,4 +1,3 @@
-let previewer = document.getElementById("previewer").style
 let wps = document.querySelectorAll(".wplist li > img")
 
 for (let i = 0; i < wps.length; i++) {
@@ -20,25 +19,9 @@ for (let i = 0; i < wps.length; i++) {
     let title = document.createElement("h2")
     title.innerText = wps[i].title
     
-    /*let previewbtn = document.createElement("a")
-    previewbtn.innerText = "Preview"
-    previewbtn.classList.add("download")
-    previewbtn.classList.add("preview")
-    previewbtn.href = "javascript:void(0)"*/
-    
     wps[i].parentElement.style.backgroundImage = `url(${wps[i].src})`
     wps[i].parentElement.appendChild(title)
     wps[i].parentElement.appendChild(dlcontainer)
     dlcontainer.appendChild(hd)
     dlcontainer.appendChild(fhd)
-    //dlcontainer.appendChild(previewbtn)
-    
-    /*previewbtn.addEventListener("click", function() {
-        previewer.backgroundImage = `url(${wps[i].src})`
-        previewer.display = "block"
-    })*/
 }
-
-document.getElementById("previewcloser").addEventListener("click", function() {
-    previewer.display = "none"
-})
